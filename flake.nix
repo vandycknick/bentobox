997ee71874f38c0311b0e0b7016805b9d032f716
Nick Van Dyck <vandyck.nick@outlook.com>
2026-02-18 08:20:17 +0100

    chore: add flake file for development

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "bentobox";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.vfkit
+            ];
+            # shellHook = ''
+            #   make install
+            # '';
+          };
+        }
+      );
+    };
+}
